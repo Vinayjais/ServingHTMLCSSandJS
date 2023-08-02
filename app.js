c9ebd58424566f52b091bed2886ed24dc78ca6c3
Vinayjais <17vinayjaiswal>
2023-08-02 18:57:20 +0530

    first Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const path = require('path');
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+
+});
+
+
+
+app.listen(4000 ,( ) => {
+    console.log("Server is running...");
+})
